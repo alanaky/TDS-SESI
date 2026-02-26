167504e9a0b48b76c1a48ca0a4b63843369f0483
alanaky <alanaescola12@gmail.com>
2026-02-26 12:13:14 -0300

    Inicio com JS

---
+++ comentarios.js
@@ -0,0 +1,9 @@
+// O console é utilizado pra imprimir algo na tela
+
+console.log("Sesi");
+console.log("Senai");
+
+/* 
+console.log("Senai");
+console.log("Senai");
+*/
---
+++ desafio_bloco.js
@@ -0,0 +1,12 @@
+/*
+Desafio: Criar dois blocos de código.
+Onde o primeiro deve imprimir na tela os números pares de 0 a 10 ne o segundo bloco deve imprimir na tela os impares. */
+
+
+{
+    console.log("Números Pares: 2, 4, 6, 8, 10")
+}
+    
+{
+    console.log("Números Impares: 1, 3, 5, 7, 9")
+}
