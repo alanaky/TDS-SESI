8dd478476c12ffa9f8c57a883833c6f4031a8332
alanaky <alanaescola12@gmail.com>
2026-03-26 11:56:06 -0300

    Atividade 2

---
+++ atividades.js/atividade2.js
@@ -0,0 +1,6 @@
+const idade_human= 10;
+let resultado;
+
+resultado = idade_human * 7;
+
+console.log(`Se a idade de um humano for ${idade_human}, sua idade equivale a ${resultado} anos de um cachorro.`);
