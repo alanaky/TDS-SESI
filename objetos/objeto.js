32e284d952bd2ba535058a7029c42f1de6bf1bb5
alanaky <alanaescola12@gmail.com>
2026-03-26 10:54:29 -0300

    objetos em js

---
+++ objetos/objeto.js
@@ -0,0 +1,16 @@
+const aluno = {
+    nome: "Alana",
+    idade: 16,
+    curso: "Desenvolvimento de Sistemas"
+}
+
+console.log(aluno);
+console.log(aluno.nome);
+console.log(aluno["idade"]);
+
+aluno.matricula = "2025A"; // adicionar
+aluno.idade = 18; // alterar
+
+delete aluno.curso; // remover
+
+console.log(aluno);
