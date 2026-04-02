53589ef7ed68105f09a857ec74509c9fc8be4421
alanaky <alanaescola12@gmail.com>
2026-04-02 11:36:56 -0300

    Atividade 6

---
+++ atividades.js/atividade6.js
@@ -0,0 +1,11 @@
+let nota1 = 3;
+let nota2 = 4;
+media = (nota1 + nota2) / 2;
+
+if(media >= 7){
+    console.log("Aprovado");
+} else if (media <=6.9 && media >= 5){
+    console.log("Recuperação");
+} else {
+    console.log("Reprovado");
+}
