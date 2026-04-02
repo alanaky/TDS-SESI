8c05bf7b5a14a768a0ef81f257122e5fd942dd9b
alanaky <alanaescola12@gmail.com>
2026-04-02 11:53:43 -0300

    Atividade 10

---
+++ atividades.js/atividade10.js
@@ -0,0 +1,7 @@
+const hardware = ["Placa de Vídeo", "Processador", "Monitor", "Memória Ram", "Ventuinha"];
+
+hardware.push("SSD");
+console.log(hardware);
+
+hardware.shift(0);
+console.log(hardware);
