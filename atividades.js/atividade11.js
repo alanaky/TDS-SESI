4e4afb9d8781e0d65cd41c9d1b528e838a0def17
alanaky <alanaescola12@gmail.com>
2026-04-02 12:07:37 -0300

    Atividade 11

--- atividades.js/atividade11.js
+++ atividades.js/atividade11.js
@@ -1,6 +1,6 @@
 const nomes = ["Bianca", "Yohanna", "Julia", "Carlos", "Laura", "Diossanys", "Elena", "Bruno", "Felipe", "Julio"]
 
-if(nomes.includes("Alana") == true){
+if(nomes.includes("Alana")){
     console.log("Usuário encontrado no sistema");
 } else {
     console.log("Usuário não localizado");
