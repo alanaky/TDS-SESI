6db95dcdc6828d8248b1f9920c331f0c99055c45
alanaky <alanaescola12@gmail.com>
2026-04-02 11:53:31 -0300

    Atividade 9

---
+++ atividades.js/atividade9.js
@@ -0,0 +1,14 @@
+let tempo = 10;
+let nome = "Alana";
+
+function horaAtual (tempo, nome) {
+if(tempo >= 0 && tempo <=11){
+    console.log(`Bom dia ${nome}`);
+} else if (tempo = 12 && tempo <=17){
+    console.log(`Boa tarde ${nome}`);
+} else {
+    console.log(`Boa noite ${nome}`);
+    }
+}
+ 
+horaAtual(10, "Alana");
